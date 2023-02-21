805641f99bf12af662fbcb06e584edb7e34438ae
Andrés Torres <atorres@medialima.mx>
2023-02-20 21:23:05 -0600

    Persona

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,12 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Persona;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -18,6 +19,34 @@ public class HelloWorldController {
         Map<String,String> response= new HashMap<>();
         response.put("message","Hello world");
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+    @GetMapping("/persona")
+    public ResponseEntity<Persona> getPersona(){
+        Persona p= new Persona();
+        p.setNombre("Andres");
+        p.setEdad(33);
+        return new ResponseEntity<>(p, HttpStatus.OK);
+    }
+    @GetMapping("/persona/list")
+    public ResponseEntity<List<Persona>> getPersonas(){
+        Persona p= new Persona();
+        p.setNombre("Andres");
+        p.setEdad(33);
+        Persona p2= new Persona();
+        p2.setNombre("Juanito");
+        p2.setEdad(20);
+        List<Persona> personaList= new ArrayList<>();
+        personaList.add(p);
+        personaList.add(p2);
+        return new ResponseEntity<>(personaList, HttpStatus.OK);
+    }
+
+    @PostMapping("/persona/save")
+    public ResponseEntity<Persona> savePersona(@RequestBody Persona p){
+        p.getNombre();
+        p.getEdad();
+        //Llamada al metodo de nuestro servicio que va a guardar a una persona
+        return new ResponseEntity<>(p,HttpStatus.OK);
     }
 
 }
